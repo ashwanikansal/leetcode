fb61cc8be5891d2b5e515bdc318094cbff3cd9a5
Ashwani Kansal <74704355+ashwanikansal@users.noreply.github.com>
2026-04-10 18:15:50 +0530

    Time: 185 ms (12.90%) | Memory: 49.3 MB (24.22%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -1,7 +1,6 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-        print(intervals)
         last_point = float("-inf")
         res = 0
 
