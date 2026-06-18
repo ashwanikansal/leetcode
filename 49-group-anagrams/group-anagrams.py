339180c5755ab2c0e7344817c6bf8f445caed44e
Ashwani Kansal <74704355+ashwanikansal@users.noreply.github.com>
2026-06-18 23:25:29 +0530

    Time: 11 ms (83.67%) | Memory: 22.5 MB (43.69%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,30 +1,11 @@
 class Solution:
-    def createKey(self, s):
-        hasharray = [0]*26
-        for ch in s:
-            hasharray[ord(ch)-ord('a')] += 1
-        return tuple(hasharray)
-
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
-        # 2 words are anagram or not?
-        # length should be same
-        # freq of each character should be same in both word.
+        anagrams = {}
 
-        dictionary = {} # keys: "0104010... 26length" : value: [strings]
-        
-        for string in strs:
-            key = self.createKey(string)
-            dictionary.setdefault(key, []).append(string)
-        
-        print(dictionary)
-        
-        res = []
-        
-        for k in dictionary:
-            res.append(dictionary[k])
+        for s in strs:
+            key = tuple(sorted(s))
+            anagrams.setdefault(key, []).append(s)
 
-        return res
-
-
+        return list(anagrams.values())
         
