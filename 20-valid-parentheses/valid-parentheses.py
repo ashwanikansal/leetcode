7bec6ae2240880eea1374d4f1f6507e244064285
Ashwani Kansal <74704355+ashwanikansal@users.noreply.github.com>
2026-07-14 00:03:09 +0530

    Time: 19 ms (4.04%) | Memory: 19.3 MB (65.28%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        for ch in s:
+            if ch == '{' or ch == '(' or ch == '[':
+                stk.append(ch)
+            else:
+                if len(stk) == 0:
+                    return False
+                if (ch == ')' and stk[-1] == '(') or (ch == '}' and stk[-1] == '{') or (ch == ']' and stk[-1] == '['):
+                    stk = stk[:-1]
+                else:
+                    return False
+        return len(stk) == 0
